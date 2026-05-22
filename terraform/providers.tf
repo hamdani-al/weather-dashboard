ecd47601e2b2e652f40ccf2fc399388b9b10e77a
hamdani-al <kenwayabdullah@gmail.com>
2026-05-22 15:04:19 +0100

    Add GitHub Actions CI/CD workflow and remote state backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,15 +2,22 @@ terraform {
   required_version = ">= 1.5.0"
 
   required_providers {
-  azurerm = {
-    source  = "hashicorp/azurerm"
-    version = "~> 4.0"
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6"
+    }
   }
-  random = {
-    source  = "hashicorp/random"
-    version = "~> 3.6"
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstateweatherkenwa1"
+    container_name       = "tfstate"
+    key                  = "weather-dashboard.tfstate"
   }
-}
 }
 
 provider "azurerm" {
